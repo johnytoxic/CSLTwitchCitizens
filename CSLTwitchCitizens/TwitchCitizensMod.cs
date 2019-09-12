99747d0b73e8dd13efb87cd2c7db1db7f413b348
johnytoxic <johnytoxic@protonmail.com>
2019-09-12 16:29:56 +0200

    init: create Cities: Skyline mod

---
+++ CSLTwitchCitizens/TwitchCitizensMod.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace CSLTwitchCitizens
+{
+    public class TwitchCitizensMod: IUserMod
+    {
+        public string Name => "Twitch Citizens";
+        public string Description => "Integrate your Twitch.tv viewers into Cities: Skylines";
+    }
+}
